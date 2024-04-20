ca5c20f05dc712a98f91079edf84f220cb62bb53
Sekretaer <140841214+Sekretaer@users.noreply.github.com>
2024-04-20 18:24:56 +0200

    added files

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".js-btn")
+btn.addEventListener("click", function(){
+    const tour = document.querySelector(".last");
+    tour.classList.toggle("invisible")
+    btn.classList.toggle("rotated")
+})
